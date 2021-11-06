30006d7657c7810425424a7d1dd17e6f2dad8f1a
Diego Andrés <diegoandres_cortes@outlook.com>
2021-11-06 09:32:09 -0600

    gh release
    Signed-off-by: Diego Andrés <diegoandres_cortes@outlook.com>

---
+++ languages/Settings.english.php
@@ -0,0 +1,9 @@
+<?php
+// Version: 2.0 RC4; Settings
+
+global $settings;
+
+$txt['theme_thumbnail_href'] = $settings['images_url'] . '/thumbnail.gif';
+$txt['theme_description'] = 'HabboLand Theme by <a href="https://smftricks.com">SMF Tricks</a>';
+
+?>
---
+++ languages/ThemeStrings.english.php
@@ -0,0 +1,11 @@
+<?php
+// Version: 2.0; Themes
+
+$txt['slogan_default'] = 'Your Slogan goes here!';
+$txt['forum_title'] = 'The Forum Title';
+$txt['forum_title_desc'] = 'From here, you can change the "HabboLand" Logo, with you own title style. <br /> <span class="smalltext">You can\'t put any acute\'s, and any numbers ... sorry.</span>';
+$txt['title_style'] = 'The Style of the Forum Title';
+$txt['title_style_desc'] = 'From here, you can change the style of the logo, with a default styles.';
+$txt['site_slogan_desc2'] = 'Add your own Slogan Text, this will replace the text "Your Slogan goes here!"';
+
+?>
